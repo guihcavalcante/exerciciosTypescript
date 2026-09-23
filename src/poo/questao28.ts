68858e0221d6532b688946442200388084b35daa
josevitor211-byter <guihernandescavalcante@gmail.com>
2026-09-23 11:53:47 -0300

    estrutura 28 e 41

---
+++ src/poo/questao28.ts
@@ -0,0 +1,14 @@
+// 28. Gestão de Diárias de um Hotel Fazenda
+// Um hotel fazenda em Tobias Barreto quer automatizar o cálculo de suas hospedagens. Uma
+// acomodação básica possui o número do quarto e o preço base da diária. A Suíte Master possui um
+// valor adicional fixo referente ao uso da hidromassagem. O sistema deve interagir com o recepcionista
+// perguntando os dados dos quartos e quantos dias o hóspede ficou alojado. O programa calcula o valor
+// total devido de cada quarto inserido em uma lista de check-outs. Ao final, utilizando métodos de
+// busca ou filtragem, o sistema deve exibir apenas os quartos que faturaram mais de R$ 1.000,00 na
+// temporada.
+
+export function runQuestion28Poo() {
+    class Hospedagem {
+        
+    }
+}
---
+++ src/poo/questao41.ts
@@ -0,0 +1,8 @@
+// 41. Abstração Herança Polimorfismo Repetição Encapsulamento
+// Gerenciador de Encomendas de Correios
+// Um centro de distribuição precisa calcular o frete de suas entregas. A classe Encomenda possui o peso
+// em kg e a cidade de destino privados. A classe EncomendaPadrão cobra R$ 10,00 por kg. A classe
+// EncomendaExpressa cobra R$ 20,00 por kg e garante entrega em até 24 horas. O sistema solicita em
+// um laço de repetição os dados das encomendas registradas no balcão. O programa processa cada uma,
+// calcula o valor do frete utilizando o método sobrescrito nas subclasses e exibe o valor acumulado
+// cobrado em taxas de frete expresso durante o dia.
